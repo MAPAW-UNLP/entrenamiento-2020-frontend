0000d8f395d99c6fabc4d343836e977a4b4aea93
Katherine Canaza <kathe.21@outlook.es>
2020-10-12 15:38:27 -0300

    Actualización de ruta de registro de profesor.

--- entrenamientoApp/src/app/app-routing.module.ts
+++ entrenamientoApp/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//vistas 
+import { RegistroUsuarioComponent } from './registro/registro-usuario.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {path: 'usuarios/registro', component: RegistroUsuarioComponent},
+  {path: '', redirectTo:'usuarios/registro', pathMatch: 'full'}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- entrenamientoApp/src/app/app.module.ts
+++ entrenamientoApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistroUsuarioComponent } from './registro/registro-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     BrowserModule,
